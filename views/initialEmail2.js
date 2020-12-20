c984ea8854b69accae07d925b70b5e0d94868261
ADC04 <allendcooper@gmail.com>
2020-12-20 18:32:55 -0500

    fixed api route

--- views/initialEmail2.js
+++ views/initialEmail2.js
@@ -11,7 +11,7 @@ const initialEmail2 = (name, availabilityArr, email) => {
     button: "display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; border: 1px solid transparent; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out; color: #fff; background-color: #007bff; border-color: #007bff;"
   }
 
-  const buttonClickLink = `https://studyparty-server.herokuapp.com/api//confirmTest?email=${email}`
+  const buttonClickLink = `https://studyparty-server.herokuapp.com/api/confirmTest?email=${email}`
 
   return (`
     <div class="text-align: center; margin: 0 auto">
