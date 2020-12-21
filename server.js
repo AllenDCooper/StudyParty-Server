fbdb6f55e434eaec2c1feeea260fb5755b7e9cbf
ADC04 <allendcooper@gmail.com>
2020-12-21 13:39:43 -0500

    changed new test confirmation email to production

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const mongoose = require("mongoose");
 // const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const initialEmail = require('./views/initialEmail.js')
-const initialEmail2 = require('./views/initialEmail2.js')
 require("dotenv").config();
 const axios = require('axios');
 const path = require('path');
@@ -45,42 +44,7 @@ async function main(email, name, response) {
       path: __dirname + '/images/StudyParty_logo_transparent_sm.png',
       cid: 'StudyParty_logo_transparent_sm.png'
     }],
-    html: initialEmail(name), // html body
-  },
-    (error, info) => {
-      if (error) {
-        return response.status(500).json({ error: 'message' })
-      }
-      return response.json({ message: 'email sent' })
-    }
-  );
-}
-
-async function testMain(email, name, availabilityArr, response) {
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAILUSER,
-      pass: process.env.EMAILPASSWORD,
-    },
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"StudyParty (TEST)" <info@mystudyparty.com>', // sender address
-    to: email, // list of receivers
-    subject: "Welcome to StudyParty! (TEST)", // Subject line
-    text: "Thanks for signing up! We're working on your request and hope to connect you with a GMAT study partner within the next 48 hours. If you need to update your availability in the interim, please respond to this email and let us know what time slots work (or don't work) with your schedule. Cheers, Team StudyParty ", // plain text body
-    attachments: [{
-      filename: 'StudyParty_logo_transparent_sm.png',
-      path: __dirname + '/images/StudyParty_logo_transparent_sm.png',
-      cid: 'StudyParty_logo_transparent_sm.png'
-    }],
-    html: initialEmail2(name, availabilityArr, email), // html body
+    html: initialEmail(name, availabilityArr, email), // html body
   },
     (error, info) => {
       if (error) {
@@ -123,22 +87,15 @@ app.get('/api/signup', (req, res) => {
 app.post('/api/signup', (req, res, next) => {
   // console.log(req);
   console.log(req.body.email);
-  // sendEmail(req.body.email, req.body.name)
-  main(req.body.email, req.body.name, res)
-});
-
-app.post('/api/signupTest', (req, res, next) => {
-  // console.log(req);
-  console.log(req.body.email);
   console.log(req.body.availabilityArr)
   // sendEmail(req.body.email, req.body.name)
-  testMain(req.body.email, req.body.name, req.body.availabilityArr, res)
+  main(req.body.email, req.body.name, req.body.availabilityArr, res)
 });
 
-app.post('/api/confirmTest', (req, res, next) => {
+app.post('/api/confirm', (req, res, next) => {
   console.log(req.query.email);
   // console.log(req.body.email);
-  console.log("confirmTest api hit")
+  console.log("confirm api hit")
   // sendEmail(req.body.email, req.body.name)
   sendConfirmToGoogle(req.query.email, res);
 });
